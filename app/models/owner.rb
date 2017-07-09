996a38f8dedd041db454839a46b08e06d8d13ac8
NicoleDVH <nicole12909@gmail.com>
2017-07-09 19:21:02 -0400

    Added validations - getting name can't be blank error

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,8 @@
 class Owner < ApplicationRecord
 	has_many :restaurants
+	validates_presence_of :name
+	validates_presence_of :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :owners
+  devise_for :owners, :controllers => { registrations: 'registrations' }
   resources :restaurants
   root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

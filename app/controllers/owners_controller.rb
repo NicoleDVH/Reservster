f52467951bb427b69b63868987c136f6e7d1fa98
NicoleDVH <nicole12909@gmail.com>
2017-07-10 12:03:10 -0400

    Changed registrations controller to owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController < Devise::RegistrationsController
+class OwnersController < ApplicationController
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :owners, :controllers => { registrations: 'registrations' }
+  devise_for :owners
   resources :restaurants
   root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
